a2e75c0774587677d266c2c5834b77249184a027
Ankit kumar <128515578+AK2105443@users.noreply.github.com>
2024-03-16 16:42:18 +0530

    Time: 11 ms (42.17%), Space: 10.4 MB (10.95%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -2,24 +2,28 @@ class Solution {
 public:
     bool isPalindrome(int x) {
         
-        if(x<0){
-            return false;
-        }
+//         if(x<0){
+//             return false;
+//         }
         
-        long long rev=0;
-        long long tmp=x;
-        while(tmp!=0){
-            int dizit=tmp%10;
-            rev=rev*10+dizit;
-            tmp=tmp/10;
-        }
+//         long long rev=0;
+//         long long tmp=x;
+//         while(tmp!=0){
+//             int dizit=tmp%10;
+//             rev=rev*10+dizit;
+//             tmp=tmp/10;
+//         }
         
-        if(rev==x){
-            return true;
-        }
+//         if(rev==x){
+//             return true;
+//         }
         
-        return false;
+//         return false;
         
+        string str=to_string(x);
+        string cmp=str;
+        reverse(cmp.begin(),cmp.end());
+        return (cmp==str);
         
     }
 };
